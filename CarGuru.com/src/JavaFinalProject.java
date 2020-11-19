f00df94dff595021b16fcc9b7afd2a31ca7920ba
anani.vossah82 <anani.vossah82@gmail.com>
2020-11-19 07:21:52 -0600

    added interface and method message for sedan, SUV and Truck.

---
+++ CarGuru.com/src/JavaFinalProject.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.JavaTeam.CarGuru.com;
+
+/**
+ * @author anani
+ *
+ */
+public class Truck implements Car {
+
+	/**
+	 * 
+	 */
+	
+	 @Override
+	public void carMessage() {
+		
+	System.out.println("A Truck could be ideal for customers with contruction business needs");
+		
+	}
+
+	 
+	 public void alert() {
+			System.out.println("No worries, this truck certainly looks good on you!");
+			
+		 }
+}
