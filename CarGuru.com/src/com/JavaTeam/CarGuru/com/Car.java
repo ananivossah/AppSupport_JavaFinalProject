9348017396c06f8019522f0db717bfa2cab64d01
anani.vossah82 <anani.vossah82@gmail.com>
2020-11-19 20:29:11 -0600

    added getters and setters for brand

--- CarGuru.com/src/com/JavaTeam/CarGuru/com/Car.java
+++ CarGuru.com/src/com/JavaTeam/CarGuru/com/Car.java
@@ -10,27 +10,34 @@ package com.JavaTeam.CarGuru.com;
 public interface Car {
 	
 	
-	/* String Brand = "";
-	 
+	String Brand = "";
 	String model = "";
 	int year = 0;
 	double price = 0;
-	String color = "";
 	double mileage = 0;
-	enum carType{
-		Sedan,
-		SUV,
-		Truck
-	};
-	enum driveTrain{};
-	enum Transmission{};
+	// enum color = "";
 	
-*/
+	// enum driveTrain{};
+	//enum Transmission{};
+	
+
+	public void getBrand();
+			
+	public void setBrand();
+		
+			
+	
+	
+	
+	
 	
 	// interface method
 	public void carMessage();
 	
 	public void alert();
+	
+	
+	
 	
 	
 }
